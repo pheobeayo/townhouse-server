77eb86e9c0d6b7b1bfcfb220a33a4a889ece014b
imrany <imranmat254@gmail.com>
2024-05-03 14:16:31 +0300

    passport middleware

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes"
+import passport from "passport"
+import session from "express-session"
 import { Server } from "socket.io"
 import websocket from "./websocket"
 import drive from "./drive"
+import googleOAuth from "./auth/google"
 import { config } from "dotenv";
 config();
 
 const app=express();
+
+// Initialize Passport and use sessions
+app.use(session({ 
+    secret: `${process.env.JWT_SECRET}`, 
+    resave: true, 
+    saveUninitialized: true 
+}));
+//app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.authenticate('session'));
+
 const cors_option = {
   origin:["http://localhost:3000"],
   methods: ["GET", "POST", "DELETE", "UPDATE", "PATCH", "PUT"]
@@ -19,6 +33,7 @@ app.use(cors(cors_option))
 app.use(express.urlencoded({extended:false}))
 app.use('/api',router)
 app.use('/drive',drive)
+app.use('/auth/googleOAuth',googleOAuth)
 
 let port=process.env.PORT||8000
 let server=app.listen(port,()=>{
