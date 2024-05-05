3f2dac0142ff7db44f5d31416b6e438a204092f9
imrany <imranmat254@gmail.com>
2024-05-05 14:08:39 +0300

    deciding if i'll use nodemail or gmail API

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,43 @@
 import pool from "../pg"
+import { createTransport } from "nodemailer"
+import {genSalt, compare, hash} from "bcryptjs"
+import { verify, sign } from "jsonwebtoken"
+
+function createVerificationCode(id:string){
+    let date=new Date()
+    let min=date.getMinutes()<10?`0${date.getMinutes()}`:date.getMinutes()
+    let code=`${min}${date.getFullYear()}`
+}
+
+export async function verifyEmail(req:any,res:any){
+    try{
+        const {email}=req.body
+        pool.query('SELECT * FROM users WHERE email = $1',[email],(error,results)=>{
+        if(!results.rows[0]){
+            let mailTransporter=createTransport({
+                service:'gmail',
+                auth:{
+
+                }
+            })
+        }else{}
+        })
+    }catch(error:any){
+        res.status(501).send({error:error.message})
+    }
+}
 
 export async function createAccount(req:any,res:any){
     try{
         const {}=req.body
+    }catch(error:any){
+        res.status(501).send({error:error.message})
+    }
+}
+
+export async function Login(req:any,res:any){
+    try{
+        const {email, password}=req.body
     }catch(error:any){
         res.status(501).send({error:error.message})
     }
