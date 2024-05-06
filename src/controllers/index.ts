bdaa8460f38775649314fb09b9adf753765536a3
imrany <imranmat254@gmail.com>
2024-05-06 09:04:32 +0300

    fixing errors

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -205,11 +205,12 @@ export async function getUserDetails(req:any,res:any){
 export async function authenticateUserWithAccessToken(req:any,res:any){
     try{
         let {access_token}=req.params
-        pool.query('SELECT * FROM users WHERE access_token =$1 AND provider=$2 RETURNING *',[access_token,'google'],(error,results)=>{
+        pool.query('SELECT * FROM users WHERE access_token =$1 AND provider=$2',[access_token,'google'],(error,results)=>{
             if(error){
                 console.log(error)
                 res.status(501).send({error:error})
             }else{
+                console.log(results)
                 if(!results.rows[0]){
                     res.status(404).send({error:`This account does not exist!`})
                 }else{
