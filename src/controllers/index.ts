a3ddc666cbe712e6c98af98cdb7144f8177d43e7
imrany <imranmat254@gmail.com>
2024-05-06 08:54:39 +0300

    fixing errors

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -210,6 +210,7 @@ export async function authenticateUserWithAccessToken(req:any,res:any){
                 console.log(error)
                 res.status(501).send({error:error})
             }else{
+                console.log(results.rows[0])
                 if(!results.rows[0]){
                     res.status(404).send({error:`This account does not exist!`})
                 }else{
