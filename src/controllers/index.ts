17efc0f7547542293fa0fd32f1ff0cd12474776f
imrany <imranmat254@gmail.com>
2024-05-07 13:52:38 +0300

    fixing errors

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -26,16 +26,26 @@ function createVerificationCode(){
 
 async function sendEmail(emailTo:any,subject:string,text:string){
     try{
-        let email={
-            to:`${emailTo}`,
-            from:`${process.env.TRANSPORTER_EMAIL}`,
-            subject,
-            text
-        }
-        
+        // You can use UTF-8 encoding for the subject using the method below.
+        // You can also just use a plain string if you don't need anything fancy.
+        const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString('base64')}?=`;
+        const messageParts = [
+            `From: Townhouse <${process.env.TRANSPORTER_EMAIL}>`,
+            `To: <${emailTo}>`,
+            'Content-Type: text/html; charset=utf-8',
+            'MIME-Version: 1.0',
+            `Subject: ${utf8Subject}`,
+            '',
+            `${text}`,
+  ];
+  const message = messageParts.join('\n');
+         // The body needs to be base64url encoded.
+        const encodedMessage = Buffer.from(message).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
         let result=await gmail.users.messages.send({
             userId:`${process.env.TRANSPORTER_EMAIL}`,
-            resource:email,
+        requestBody: {
+            raw: encodedMessage,
+        },
         })
 
         console.log("email sent successfully", result.data)
